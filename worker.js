ecaa5251709f936f46cd54ef24a3a4b0f14cab41
Nathan Clevenger <nateclev@gmail.com>
2022-09-27 07:24:37 -0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,12 @@
+import { pluck } from 'lodash'
+
+export default {
+  fetch: async (req, env) => {
+    const { hostname, pathname, search } = new URL(req.url)
+    const [args, ...rest] = pathname.split('/')
+    const url = req.url.replace(`${hostname}/${args}/`,'')
+    const data = await fetch(), req).then(res => res.json()).catch(({ name, message }) => ({ error: { name, message }}))
+    const pluckedData = pluck(data, args)
+    return new Response(JSON.stringify(pluckedData, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  },
+}
